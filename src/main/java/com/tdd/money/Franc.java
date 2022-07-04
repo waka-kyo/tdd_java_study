58e45c892ad9d5824a32237ccd30937a84ea24dd
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-04 14:32:32 +0900

    p40 変数名もmoneyにする

--- src/main/java/com/tdd/money/Franc.java
+++ src/main/java/com/tdd/money/Franc.java
@@ -11,7 +11,7 @@ public class Franc extends Money{
   }
 
   public boolean equals(Object object) {
-    Money franc = (Money) object;
-    return amount == franc.amount;
+    Money money = (Money) object;
+    return amount == money.amount;
   }
 }
