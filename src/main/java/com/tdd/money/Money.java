5931fce7b7684f7ed255afb7c544286075164487
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-04 14:25:53 +0900

    p38 equalsメソッドをDollarからMoneryに引き上げる

--- src/main/java/com/tdd/money/Money.java
+++ src/main/java/com/tdd/money/Money.java
@@ -2,5 +2,8 @@ package com.tdd.money;
 
 public class Money {
   protected int amount;
-
+  public boolean equals(Object object) {
+    Money money = (Money) object;
+    return amount == money.amount;
+  }
 }
