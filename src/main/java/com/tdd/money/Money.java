959cfc67fb3315363ff45cc90cc66186d9f1c3d5
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-06 14:37:04 +0900

    p49 Dollarを返すFactory MethodをMoneyに作る、テストがグリーンになる

--- src/main/java/com/tdd/money/Money.java
+++ src/main/java/com/tdd/money/Money.java
@@ -2,9 +2,14 @@ package com.tdd.money;
 
 public class Money {
   protected int amount;
+
   public boolean equals(Object object) {
     Money money = (Money) object;
     return amount == money.amount
         && getClass().equals(money.getClass());
+  }
+
+  public static Dollar Dollar(int amount) {
+    return new Dollar(amount);
   }
 }
