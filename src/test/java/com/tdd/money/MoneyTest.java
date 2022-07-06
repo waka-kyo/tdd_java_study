f3f6a1ee8227bf17602e731a5d241ff7b0dbeea4
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-06 14:43:48 +0900

    p50 テストが通っているので、残りもFactory Methodを使うように変更

--- src/test/java/com/tdd/money/MoneyTest.java
+++ src/test/java/com/tdd/money/MoneyTest.java
@@ -11,16 +11,16 @@ public class MoneyTest {
   @Test
   public void testMultiplication(){
     Money five = Money.Dollar(5);
-    assertEquals(new Dollar(10), five.times(2));
-    assertEquals(new Dollar(15), five.times(3));
+    assertEquals(Money.Dollar(10), five.times(2));
+    assertEquals(Money.Dollar(15), five.times(3));
   }
   @Test
   public void testEquality() {
-    assertTrue(new Dollar(5).equals(new Dollar(5)));
-    assertFalse(new Dollar(5).equals(new Dollar(6)));
+    assertTrue(Money.Dollar(5).equals(Money.Dollar(5)));
+    assertFalse(Money.Dollar(5).equals(Money.Dollar(6)));
     assertTrue(new Franc(5).equals(new Franc(5)));
     assertFalse(new Franc(5).equals(new Franc(6)));
-    assertFalse(new Franc(5).equals(new Dollar(5)));
+    assertFalse(new Franc(5).equals(Money.Dollar(5)));
   }
   @Test
   public void testFrancMultiplication(){
