198e4e7b5fca08531d96e2dd54063859bea96516
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-11 14:38:38 +0900

    p56 2つのサブクラスでそれぞれcurrencyメソッドを実装する

--- src/main/java/com/tdd/money/Dollar.java
+++ src/main/java/com/tdd/money/Dollar.java
@@ -5,7 +5,9 @@ class Dollar extends Money{
   Dollar(int amount) {
     this.amount = amount;
   }
-
+  String currency() {
+    return "USD";
+  }
   Money times(int multiplier){
     return new Dollar(amount * multiplier);
   }
--- src/main/java/com/tdd/money/Franc.java
+++ src/main/java/com/tdd/money/Franc.java
@@ -5,7 +5,9 @@ public class Franc extends Money{
   Franc(int amount) {
     this.amount = amount;
   }
-
+  String currency() {
+    return "CHF";
+  }
   Money times(int multiplier){
     return new Franc(amount * multiplier);
   }
