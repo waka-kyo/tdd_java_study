4ae38b795fda2d7ba2e574d1cd3eba26d0f2aa70
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-11 17:22:01 +0900

    p60 Franc.timesがコンストラクタを呼び出しているので、FactoryMethodを使うように修正

--- src/main/java/com/tdd/money/Franc.java
+++ src/main/java/com/tdd/money/Franc.java
@@ -6,6 +6,6 @@ public class Franc extends Money{
     this.currency = "CHF";
   }
   Money times(int multiplier){
-    return new Franc(amount * multiplier, null);
+    return Money.franc(amount * multiplier);
   }
 }
