97b9ef091dd201a555b1726346af35ac8be0984e
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-12 14:18:10 +0900

    p67 Franc型とMoney型の区別は不要に思われるため、timesメソッドでMoneyをnewするようにしてみる

--- src/main/java/com/tdd/money/Franc.java
+++ src/main/java/com/tdd/money/Franc.java
@@ -5,6 +5,6 @@ class Franc extends Money{
     super(amount, currency);
   }
   Money times(int multiplier){
-    return new Franc(amount * multiplier, currency);
+    return new Money(amount * multiplier, currency);
   }
 }
