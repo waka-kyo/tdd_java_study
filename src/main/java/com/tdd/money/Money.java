f579b3ffe95964b7e09c568ad0205425da09593d
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-12 14:32:03 +0900

    p71 2つのtimesメソッドの実装が完全に一致したので、親クラスに引き上げる

--- src/main/java/com/tdd/money/Money.java
+++ src/main/java/com/tdd/money/Money.java
@@ -8,7 +8,7 @@ public class Money {
     this.currency = currency;
   }
   Money times(int multiplier){
-    return null;
+    return new Money(amount * multiplier, currency);
   };
   String currency(){
     return currency;
