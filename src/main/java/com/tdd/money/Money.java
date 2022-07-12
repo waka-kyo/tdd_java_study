617daeac04456d3dedf174e2fc5b7e5c2fe97deb
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-12 14:29:11 +0900

    p70 比較したいのはクラスではなくcurrencyであるので修正する、テストが通るようになる

--- src/main/java/com/tdd/money/Money.java
+++ src/main/java/com/tdd/money/Money.java
@@ -16,7 +16,7 @@ public class Money {
   public boolean equals(Object object) {
     Money money = (Money) object;
     return amount == money.amount
-        && getClass().equals(money.getClass());
+        && currency().equals(money.currency());
   }
   public String toString() {
     return amount + " " + currency;
