e306c6ff3487bff7b8d928bd1a127c524784c043
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-12 14:15:03 +0900

    p66 Francクラスのインスタンス変数currencyは常に"CHF"なので書き換える

--- src/main/java/com/tdd/money/Franc.java
+++ src/main/java/com/tdd/money/Franc.java
@@ -5,6 +5,6 @@ class Franc extends Money{
     super(amount, currency);
   }
   Money times(int multiplier){
-    return new Franc(amount * multiplier, "CHF");
+    return new Franc(amount * multiplier, currency);
   }
 }
