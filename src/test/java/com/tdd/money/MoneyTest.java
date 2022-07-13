3af56afbfe095fe1182ffd3d9d9b5bf22a8bf24f
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-13 14:33:03 +0900

    p83 Expressionに為替レートを適用することによって得られる換算結果reducedを定義し、それを使ってテストをしたい

--- src/test/java/com/tdd/money/MoneyTest.java
+++ src/test/java/com/tdd/money/MoneyTest.java
@@ -29,5 +29,7 @@ public class MoneyTest {
   public void testSimpleAddition() {
     Money sum = Money.dollar(5).plus(Money.dollar(5));
     assertEquals(Money.dollar(10), sum);
+    Money reduced = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(10), reduced);
   }
 }
