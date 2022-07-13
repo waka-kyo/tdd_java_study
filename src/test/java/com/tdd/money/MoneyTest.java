704889bcbe94a3fdeada881e063eb87936c7940d
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-13 14:34:29 +0900

    p84 式ではなく銀行に仕事をさせたいので、Bankオブジェクトを作る

--- src/test/java/com/tdd/money/MoneyTest.java
+++ src/test/java/com/tdd/money/MoneyTest.java
@@ -29,6 +29,7 @@ public class MoneyTest {
   public void testSimpleAddition() {
     Money sum = Money.dollar(5).plus(Money.dollar(5));
     assertEquals(Money.dollar(10), sum);
+    Bank bank = new Bank();
     Money reduced = bank.reduce(sum, "USD");
     assertEquals(Money.dollar(10), reduced);
   }
