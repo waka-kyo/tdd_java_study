8c784357477fa451146cd25b4c7643616e832063
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-19 14:40:54 +0900

    p93 reduceメソッドの中身をSumクラスに移す

--- src/main/java/com/tdd/money/Sum.java
+++ src/main/java/com/tdd/money/Sum.java
@@ -7,4 +7,8 @@ public class Sum implements Expression{
     this.augend = augend;
     this.addend = added;
   }
+  Money reduce(String to) {
+    int amount = augend.amount + addend.amount;
+    return new Money(amount, to);
+  }
 }
