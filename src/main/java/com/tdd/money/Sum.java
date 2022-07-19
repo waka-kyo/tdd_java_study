8a32f99cc1cba329f56f4cba185268ca32beb71a
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-19 14:27:30 +0900

    p92 コンストラクタで引数をフィールドに入れ忘れているので直す

--- src/main/java/com/tdd/money/Sum.java
+++ src/main/java/com/tdd/money/Sum.java
@@ -3,5 +3,8 @@ package com.tdd.money;
 public class Sum implements Expression{
   Money augend;
   Money addend;
-  Sum(Money augend, Money added){}
+  Sum(Money augend, Money added){
+    this.augend = augend;
+    this.addend = added;
+  }
 }
