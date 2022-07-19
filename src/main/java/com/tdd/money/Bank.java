df5dc6483c79b6d0b7e6939aa2bc36d6d00b068c
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-19 14:45:45 +0900

    p94 テストを追加し、無理やりグリーンになる実装を行う

--- src/main/java/com/tdd/money/Bank.java
+++ src/main/java/com/tdd/money/Bank.java
@@ -2,6 +2,8 @@ package com.tdd.money;
 
 public class Bank {
   Money reduce(Expression source, String to) {
+    if (source instanceof Money)
+      return (Money) source;
     Sum sum = (Sum) source;
     return sum.reduce(to);
   }
