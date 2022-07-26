be73b52f0af60636e07e5b981bc6d55ea5e41f84
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-26 15:08:30 +0900

    p106 為替レートを格納させる

--- src/main/java/com/tdd/money/Bank.java
+++ src/main/java/com/tdd/money/Bank.java
@@ -1,6 +1,10 @@
 package com.tdd.money;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+  private Map<Pair, Integer> rates = new HashMap<>();
   Money reduce(Expression source, String to) {
     return source.reduce(this, to);
   }
