0f5f2b7cdcc5f2924cb8b5175bc1d233e6bc44ab
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-26 14:44:58 +0900

    p104 為替レートはbank引数に問い合わせるようにする

--- src/main/java/com/tdd/money/Money.java
+++ src/main/java/com/tdd/money/Money.java
@@ -31,7 +31,7 @@ public class Money implements Expression{
     return new Sum(this, added);
   }
   public Money reduce(Bank bank, String to) {
-    int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    int rate = bank.rate(currency, to);
     return new Money(amount / rate, to);
   }
 }
