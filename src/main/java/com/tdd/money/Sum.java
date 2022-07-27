ab9078d5718e0b243095088148ff5714e42cd1d4
Kyoko.Wakata <wakakyo@zozo.com>
2022-07-27 14:31:14 +0900

    p115 MoneyをExpressionに置き換えていく、まずはSumクラスのaugendとaddend テストがグリーンであることを確認しながらすすむ

--- src/main/java/com/tdd/money/Sum.java
+++ src/main/java/com/tdd/money/Sum.java
@@ -1,8 +1,8 @@
 package com.tdd.money;
 
 public class Sum implements Expression{
-  Money augend;
-  Money addend;
+  Expression augend;
+  Expression addend;
   Sum(Money augend, Money added){
     this.augend = augend;
     this.addend = added;
